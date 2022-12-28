7ccfc1666663b20d4cf1b3343fd9c3c64fc84c27
Petech <petekshamitbeyli@gmail.com>
2022-12-29 02:09:17 +0300

    searchbar

--- src/components/SearchBar/Searchbar.js
+++ src/components/SearchBar/Searchbar.js
@@ -1,5 +1,26 @@
-function Searchbar() {
-  return <div>Searchbar</div>;
-}
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-export default Searchbar;
+export default function Searchbar() {
+  const [term, setTerm] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    navigate(`/search?q=${term}`);
+  };
+
+  return (
+    <div className="searchbar">
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="search">Search:</label>
+        <input
+          type="text"
+          id="search"
+          onChange={(e) => setTerm(e.target.value)}
+          required
+        />
+      </form>
+    </div>
+  );
+}
